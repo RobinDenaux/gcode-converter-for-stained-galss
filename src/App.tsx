d23a42e79f0f414de2abb4099c7ebd0ca1ebb206
robicop <robicopstudio@gmail.com>
2024-04-11 23:45:30 +0200

    added favicon & popup explaining what this tool is

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-
 import './App.css'
 import {GcodeEditor} from "./component/gcodeEditor/GcodeEditor.tsx";
+import {InfoPopup} from "src/component/infoPopup/InfoPopup.tsx";
 
 function App() {
 
-  return (
-    <>
-      <GcodeEditor/>
-    </>
-  )
+    return (
+        <>
+            <InfoPopup/>
+            <GcodeEditor/>
+        </>
+    )
 }
 
 export default App
